d3834faa9ff27dc1f85ab78bd9d9e3afba05fe75
TraceDecker <decker1041@gmail.com>
2020-01-24 10:28:40 -0700

    added tests for Card class

--- src/edu/cnm/deepdive/model/Card.java
+++ src/edu/cnm/deepdive/model/Card.java
@@ -1,14 +1,18 @@
 package edu.cnm.deepdive.model;
 
+import java.util.Objects;
+
 public class Card {
 
   private final Suit suit;
   private final Rank rank;
+  private final int hash;
 
 
   public Card(Suit suit, Rank rank) {
     this.suit = suit;
     this.rank = rank;
+    hash = Objects.hash(suit, rank);
   }
 
   public Suit getSuit() {
@@ -23,4 +27,24 @@ public class Card {
   public String toString() {
     return rank.symbol() + suit.symbol();
   }
+
+  @Override
+  public int hashCode() {
+    return hash;
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    boolean comparison = false;
+    if (obj == this) {
+      comparison = true;
+    } else if (obj instanceof Card && obj.hashCode() == hash) {
+      Card other = (Card) obj;
+      if (hash == other.hash && suit == other.suit && rank == other.rank) {
+        comparison = true;
+      }
+    }
+    return comparison;
+  }
+
 }
